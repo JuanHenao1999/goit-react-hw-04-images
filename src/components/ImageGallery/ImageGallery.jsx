dd6fa70fe5b034f5bb46e75133a99ebe696be839
JuanHenao1999 <juanhenao123kl@gmail.com>
2023-06-29 16:09:26 -0500

    se monta tarea 4 images-

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import css from './ImageGallery.module.css'
+
+export const ImageGallery = ({children}) => {
+    return (
+        <ul className={css["imageGallery"]}>
+          {children}
+        </ul>
+    )
+  }
+
+export default ImageGallery
+
+ImageGallery.propTypes = {
+  children: PropTypes.node
+}
